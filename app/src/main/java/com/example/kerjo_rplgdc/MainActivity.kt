8656b580f3a648f493d86603ba67ddd697496df9
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-07-07 15:49:25 +0700

    login and SplashScreen Design

--- app/src/main/java/com/example/kerjo_rplgdc/MainActivity.kt
+++ app/src/main/java/com/example/kerjo_rplgdc/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kerjo_rplgdc
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 1500)
     }
 }

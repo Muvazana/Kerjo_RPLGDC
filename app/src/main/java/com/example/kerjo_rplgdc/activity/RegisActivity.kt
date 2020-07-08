41a4689b03a0133efb8d4730980e7921c24f74e1
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-07-08 13:17:33 +0700

    register design update

--- app/src/main/java/com/example/kerjo_rplgdc/activity/RegisActivity.kt
+++ app/src/main/java/com/example/kerjo_rplgdc/activity/RegisActivity.kt
@@ -1,12 +1,59 @@
-package com.example.kerjo_rplgdc
+package com.example.kerjo_rplgdc.activity
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kerjo_rplgdc.R
+import kotlinx.android.synthetic.main.activity_regis.*
 
 class RegisActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regis)
+        ValidateTextInput()
+        btnRegister.setOnClickListener {
+            if (!ValidateEmail() || !ValidatePassword()) {
+                return@setOnClickListener
+            }
+        }
+    }
+
+    private fun ValidateEmail(): Boolean {
+        var txtinput = txtInputEmail.editText?.text.toString()
+        if (txtinput.isEmpty()) {
+            txtInputEmail.error = "Field can't be empty"
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(txtinput).matches()) {
+            txtInputEmail.error = "Please enter a valid email address"
+            return false
+        } else {
+            txtInputEmail.error = null
+            return true
+        }
+    }
+
+    private fun ValidatePassword(): Boolean {
+        var txtinput1 = txtInputPassword1.editText?.text.toString()
+        var txtinput2 = txtInputPassword2.editText?.text.toString()
+        var valid = true
+        if (txtinput1.isEmpty()) {
+            txtInputPassword1.error = "Field can't be empty"
+            valid = false
+        } else if (txtinput1 != txtinput2 && txtinput2 != null) {
+            txtInputPassword1.error = "Password not match"
+            txtInputPassword2.error = "Password not match"
+            valid = false
+        } else {
+            txtInputEmail.error = null
+            return true
+        }
+
+
+        return valid
+    }
+
+    private fun ValidateTextInput() {
+        txtInputEmail.editText?.setText(intent.getStringExtra("txtEmail"))
     }
 }

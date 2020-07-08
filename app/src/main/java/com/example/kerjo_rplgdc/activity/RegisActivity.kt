7c484fb89c775f99e3552dc5b9f80a285cdaf987
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-07-08 13:41:25 +0700

    register design update

--- app/src/main/java/com/example/kerjo_rplgdc/activity/RegisActivity.kt
+++ app/src/main/java/com/example/kerjo_rplgdc/activity/RegisActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kerjo_rplgdc.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kerjo_rplgdc.R
@@ -36,17 +38,25 @@ class RegisActivity : AppCompatActivity() {
     private fun ValidatePassword(): Boolean {
         var txtinput1 = txtInputPassword1.editText?.text.toString()
         var txtinput2 = txtInputPassword2.editText?.text.toString()
-        var valid = true
+        var valid: Boolean = true
+
+        txtInputPassword1.error = null
+        txtInputPassword2.error = null
+
         if (txtinput1.isEmpty()) {
             txtInputPassword1.error = "Field can't be empty"
             valid = false
-        } else if (txtinput1 != txtinput2 && txtinput2 != null) {
+        }
+
+        if (txtinput2.isEmpty()) {
+            txtInputPassword2.error = "Field can't be empty"
+            valid = false
+        }
+
+        if (txtinput1 != txtinput2 && !txtinput1.isEmpty() && !txtinput2.isEmpty()) {
             txtInputPassword1.error = "Password not match"
             txtInputPassword2.error = "Password not match"
             valid = false
-        } else {
-            txtInputEmail.error = null
-            return true
         }
 
 
@@ -55,5 +65,76 @@ class RegisActivity : AppCompatActivity() {
 
     private fun ValidateTextInput() {
         txtInputEmail.editText?.setText(intent.getStringExtra("txtEmail"))
+
+        txtInputEmail.editText?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                txtInputPassword1.error = null
+                txtInputPassword2.error = null
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.isEmpty()) {
+                    txtInputEmail.error = "Field can't be empty"
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+                    txtInputEmail.error = "Please enter a valid email address"
+                } else {
+                    txtInputEmail.error = null
+                }
+            }
+        })
+        txtInputPassword1.editText?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.toString() == txtInputPassword2.editText?.text.toString()) {
+                    txtInputPassword1.error = null
+                    txtInputPassword2.error = null
+                } else if (s.toString().isEmpty()) {
+                    txtInputPassword2.error = null
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                txtInputPassword1.error = null
+                txtInputPassword2.error = null
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.isEmpty()) {
+                    txtInputPassword1.error = "Field can't be empty"
+                } else if (s != txtInputPassword2.editText?.text.toString() && !s.isEmpty() && !txtInputPassword2.editText?.text.toString()
+                        .isEmpty()
+                ) {
+                    txtInputPassword1.error = "Password not match"
+                    txtInputPassword2.error = "Password not match"
+                }
+            }
+        })
+        txtInputPassword2.editText?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.toString() == txtInputPassword1.editText?.text.toString()) {
+                    txtInputPassword1.error = null
+                    txtInputPassword2.error = null
+                } else if (s.toString().isEmpty()) {
+                    txtInputPassword1.error = null
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.isEmpty()) {
+                    txtInputPassword2.error = "Field can't be empty"
+                } else if (s != txtInputPassword1.editText?.text.toString() && !s.isEmpty() && !txtInputPassword1.editText?.text.toString()
+                        .isEmpty()
+                ) {
+                    txtInputPassword1.error = "Password not match"
+                    txtInputPassword2.error = "Password not match"
+                }
+            }
+        })
     }
 }

e808d1038ed350cd2a64886636dddfcaa39d0270
maulanarasoky <rasoky.maulana@gmail.com>
2020-07-13 20:13:37 +0700

    Change Home Fragment View

--- app/src/main/java/com/example/kerjo_rplgdc/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/kerjo_rplgdc/fragment/HomeFragment.kt
@@ -1,18 +1,22 @@
 package com.example.kerjo_rplgdc.fragment
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.kerjo_rplgdc.R
+import com.synnapps.carouselview.ImageListener
 import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
+
+    var images = arrayOf(R.drawable.header_pic1, R.drawable.header_pic2)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +25,16 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        carouselView.pageCount = images.size
+
+        carouselView.setImageListener(imageListener)
+    }
+
+    val imageListener = ImageListener { position, imageView -> imageView?.setImageResource(images[position]) }
 
     override fun onResume() {
         super.onResume()
